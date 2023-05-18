5a32656b2758a89ee77664f5469e7a3c411a8b53
Man980 <96098332+Man980@users.noreply.github.com>
2023-05-18 14:15:43 -0400

    code_generator_exo1.py

--- exercice#1.py
+++ exercice#1.py
@@ -1,11 +1,15 @@
-#Don't forget to put in comment the other exercices when executing 
+#This app aims at generating the number of the open door among several
 
-#Exercice 1
-#ip = input("Enter you IP address\n>>> ")
-ip =input('Enter your IP address. egz: 192.168.192')
+#for example:
+
+#"127.0.0.1" will give you
+#(1+2+7+0+0+1) * 1 = 11 => So the door that is opened is the door number 11
+
+#ip = input("Enter your IP address\n>>> ")
+ip =input('Enter your IP address. egz: 192.168.192\n>>>')
 ip = ip.replace('.','')
-first_letter = int(ip[0])
+first_digit = int(ip[0])
 add =0
 for x in ip:
 	add +=int(x)
-print(add*first_letter)
+print(add*first_digit)
