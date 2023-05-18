c3fc69f84d3de5e524d2d73d0f1fdce38915b7c2
Man980 <96098332+Man980@users.noreply.github.com>
2023-05-18 14:34:07 -0400

    update description exercice#11.py

--- exercice#11.py
+++ exercice#11.py
@@ -1,5 +1,5 @@
 #Exercice 11
-# That's a method used to display the largest and the smallest number
+# That's a method used to display the largest and the smallest number wihout using the built-in functions MIN AND MAX
 int_list =[1, 5, 0, -3, -4, 9]
 new_list = sorted(int_list)
 print(new_list[0], ",", new_list[len(int_list)-1])
