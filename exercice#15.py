7e7f7ae467bf0ed5a7b4e2725d5c899e5c67f6b6
Man980 <96098332+Man980@users.noreply.github.com>
2023-05-18 14:38:36 -0400

    VLOOKUP-function

--- exercice#15.py
+++ exercice#15.py
@@ -1,10 +1,13 @@
 #Exercie 15
 
-#you have a string between two words "hidden" and "endpass", print out this string
-text1 = " I'm hidden patagtou teolito endpass you hear me"
-x = text1.split()
-position1 = x.index('hidden')
-position2 = x.index('endpass')
+#you have a string between two words "hidden" and "endpass", print out that string
+text = " I'm hidden patagtou teolito endpass you hear me"
+def vlookup(text):
+	x = text.split()
+	position1 = x.index('hidden')
+	position2 = x.index('endpass')
 
-chars = x[position1+1:position2]
-#print(" ".join(chars))
+	chars = x[position1+1:position2]
+	print(" ".join(chars))
+
+vlookup(text)
